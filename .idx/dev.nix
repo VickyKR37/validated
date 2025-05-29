26e8f2e0511ff91431afe2bd839861643a12a257
vickyks <vickyks@hotmail.com>
2025-05-29 06:51:36 +0000

    changed hosting to next in firebase.json. removed commenting out from idx file so preview would be displayed

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,6 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
- {
+{
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
 
@@ -14,7 +14,7 @@
   ];
 
   # Sets environment variables in the workspace
-  env = {};
+  env = { };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -25,16 +25,16 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+          #   # and show it in IDX's web preview panel
+          command = [ "npm" "run" "dev" ];
+          manager = "web";
+          env = {
+            #     # Environment variables to set for your server
+            PORT = "$PORT";
+          };
+        };
       };
     };
 
